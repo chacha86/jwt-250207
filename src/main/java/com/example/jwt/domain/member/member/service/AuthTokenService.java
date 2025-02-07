3a280a198b6c2037f8f52956bf3ceb257f1e5cd2
chacha86 <taejincha0619@gmail.com>
2025-02-07 12:29:06 +0900

    authTokenService & jjwt

---
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
@@ -0,0 +1,12 @@
+package com.example.jwt.domain.member.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokenService {
+
+
+}
