4d9102a57af4c8a54c7cf9da3b8010cc6ba52d7a
chacha86 <taejincha0619@gmail.com>
2025-02-11 11:40:29 +0900

    refresh accessToken

--- src/main/java/com/example/jwt/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/jwt/global/security/CustomAuthenticationFilter.java
@@ -48,14 +48,30 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         String apiKey = tokenBits[0];
         String accessToken = tokenBits[1];
 
-        Optional<Member> opMember = memberService.getMemberByAccessToken(accessToken);
+        Optional<Member> opAccMember = memberService.getMemberByAccessToken(accessToken);
 
-        if(opMember.isEmpty()) {
+        if(opAccMember.isEmpty()) {
+
+            // 재발급
+            Optional<Member> opApiMember = memberService.findByApiKey(apiKey);
+
+            if(opApiMember.isEmpty()) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String newAuthToken = memberService.getAuthToken(opApiMember.get());
+            response.addHeader("Authorization", "Bearer " + newAuthToken);
+
+
+            Member actor = opApiMember.get();
+            rq.setLogin(actor);
+
             filterChain.doFilter(request, response);
             return;
         }
 
-        Member actor = opMember.get();
+        Member actor = opAccMember.get();
         rq.setLogin(actor);
 
         filterChain.doFilter(request, response);
